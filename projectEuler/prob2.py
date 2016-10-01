caf00a261dc82e53bd007a5a7d0e047e08ee1c3c
Shane Newbill <snewbill@gmail.com>
2016-09-30 20:46:23 -0600

    Fixing inclusion of swap file

---
+++ projectEuler/prob2.py
@@ -0,0 +1,13 @@
+#sum of even valued terms in fib sequence (Values under 4 million)
+
+def fibSum(current, prev, end):
+	if current == end:
+		return current + prev
+	if current == 1:
+		return fibSum(current + prev, current, end)
+	return fibSum(current
+
+def fibEvenSum(current, end):
+	if current == end: #base case
+		if
+	if
