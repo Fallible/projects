5205f22a7b9e9dc0e411ff48a83897fb3b7a6286
Shane Newbill <snewbill@gmail.com>
2017-02-24 15:51:05 -0700

    Removing .swp.  Fixed brackets in prob1.c

--- projectEuler/prob1.c
+++ projectEuler/prob1.c
@@ -1,4 +1,4 @@
-#import <stdio.h>
+#include <stdio.h>
 
 //add up the natural numbers below 1000 that are multiples of 3 or 5
 
@@ -10,20 +10,22 @@ int isFactor(int currentNumber);
 main(){
     int sum = 0;
     int i = 0;
-    for(i; i < 1000; i++){
-        if(isFactor){
+    for(i; i < 1000; i++)
+    {
+        if(isFactor)
+        {
             sum = sum + i;
         }
     }
-
 }
 
 int isFactor(int currentNumber){
-    if(currentNumber % 5 == 0 || currentNumber % 3 == 0){
+    if(currentNumber % 5 == 0 || currentNumber % 3 == 0)
+    {
         return currentNumber;
     }
     else
     {
         return 0;
-    {
+    }
 }
