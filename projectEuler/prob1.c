3e3ac2259d24ab34b438bf3315bf7e85148b15aa
Shane Newbill <snewbill@gmail.com>
2017-02-24 13:05:35 -0700

    Prob1. Used separate function. int as bool.

---
+++ projectEuler/prob1.c
@@ -0,0 +1,29 @@
+#import <stdio.h>
+
+//add up the natural numbers below 1000 that are multiples of 3 or 5
+
+//function to know if input number is a factor of 3 or 5
+int isFactor(int currentNumber);
+
+//
+
+main(){
+    int sum = 0;
+    int i = 0;
+    for(i; i < 1000; i++){
+        if(isFactor){
+            sum = sum + i;
+        }
+    }
+
+}
+
+int isFactor(int currentNumber){
+    if(currentNumber % 5 == 0 || currentNumber % 3 == 0){
+        return currentNumber;
+    }
+    else
+    {
+        return 0;
+    {
+}
