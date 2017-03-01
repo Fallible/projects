f1cd32ba61244b74071d362008049590c61039a8
Shane Newbill <snewbill@gmail.com>
2017-02-28 22:03:09 -0700

    Started prob4.c

---
+++ projectEuler/cLang/prob4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//largest palindrome made from the product of two 3-digit numbers
+
+int checkPal(int number)
+{
+    int i;
+    int thousands, hundreds, tens, ones;
+    int segmentedNumber[4];
+    for(i = 0; i < 4; i++){
+        segmentedNumber = number %
+    }
+}
+
