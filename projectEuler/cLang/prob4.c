97d4ca1a5dfc5440fe66a20ff4fb9da2c8811953
Shane Newbill <snewbill@gmail.com>
2017-03-14 17:38:44 -0600

    Prob4 loop function

--- projectEuler/cLang/prob4.c
+++ projectEuler/cLang/prob4.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 //largest palindrome made from the product of two 3-digit numbers
 
+/*
+ *I want to build this as multiple functions.  One should check if my current
+ * number is a palindrome.  Another should run a nested for loop to generate
+ * all products of 2 three digit numbers.
+ *
+ */
+
 int checkPal(int number)
 {
     int i;
@@ -11,3 +18,19 @@ int checkPal(int number)
     }
 }
 
+
+int runLoop()
+{
+    int i = 999;
+    int j = 999;
+    int currentHighest = 0;
+    for(i; i > 99; i--)
+    {
+        for(j; j > 99; j--)
+	{
+            if(i * j > currentHighest) currentHighest = i * j;           
+	}
+    }
+    return currentHighest;
+
+}
