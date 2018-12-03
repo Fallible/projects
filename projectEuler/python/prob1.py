273703a475dff58ce083ba7ae56edc691261988a
Shane Newbill <snewbill@gmail.com>
2018-12-03 16:31:08 -0500

    Refactor project1.py. ignore .json

--- projectEuler/python/prob1.py
+++ projectEuler/python/prob1.py
@@ -1,19 +1,19 @@
 #multiples of 3 and 5
 
-def recCount(current, end):
+def rec_count(current, end):
 	if current == end:
 		return current
 	else:
-		return current + recCount(current + 1, end)
+		return current + rec_count(current + 1, end)
 
-def recMult3and5(current, end):
+def rec_mult_3and5(current, end):
 	if current == end:
 		if (current % 3 == 0 or current % 5 == 0):
 			return current
 	if (current % 5 == 0 or current % 3 == 0):
-		return current + recMult3and5(current + 1, end)
-	return recMult3and5(current + 1, end)
+		return current + rec_mult_3and5(current + 1, end)
+	return rec_mult_3and5(current + 1, end)
 	
 
-print recCount(1, 100)
-print recMult3and5(1, 100)
+print(rec_count(1, 100))
+print(rec_mult_3and5(1, 100))
