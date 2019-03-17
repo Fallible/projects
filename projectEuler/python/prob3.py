c56c29db66d85c411f2e6889fcee2c559fa98682
Shane Newbill <snewbill@gmail.com>
2019-03-17 13:32:01 -0600

    prob3 is new

--- projectEuler/python/prob3.py
+++ projectEuler/python/prob3.py
@@ -1,18 +1,32 @@
 #largest prime factor of 600851475143
 
+#keep a list of all factors found
 list_of_factors = []
+#Current number being divided
 dividend = 600851475143
+#Current number dividing into divisor
 divisor = 2
 
-def divide_by_input(dividend, divisor):
-    result = 0
-    while dividend % divisor != 0:
-        result = dividend / divisor
-    return result
+#divide the divident by the divisor. 
+#if clean division record factor, update divident
+#if !clean division, increment divisor
+def divide_by_input(dividend1, divisor2):
+    print("here")
+    if dividend1 % divisor2 == 0:
+        print("in if")
+        dividend = dividend1 / divisor2
+        print(divisor)
+        list_of_factors.append(divisor)
+    else:
+        print("in else")
+        divisor = divisor + 1
+        print(divisor)
+
 
 def main():
-    while divisor < dividend:
-        list_of_factors.append(divide_by_input(dividend, divisor))
+    while divisor < (dividend/2) + 1:
+        divide_by_input(dividend, divisor)
         print(list_of_factors)
+        print(divisor)
 
 main()
