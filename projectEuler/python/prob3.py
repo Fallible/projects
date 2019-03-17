4a47ab106ba98825d8b912fb97d487e0e4111dbf
Fallible <snewbill@gmail.com>
2019-03-17 14:32:19 -0600

    progress on prob3

--- projectEuler/python/prob3.py
+++ projectEuler/python/prob3.py
@@ -1,32 +1,35 @@
+#Python program
 #largest prime factor of 600851475143
 
-#keep a list of all factors found
-list_of_factors = []
-#Current number being divided
-dividend = 600851475143
-#Current number dividing into divisor
-divisor = 2
+def main():
+    initial_value = 600851475143
+    #keep a list of all factors found
+    list_of_factors = []
+    #Current number being divided
+    dividend = 600851475143
+    #Current number dividing into divisor
+    divisor = 2
+    
+    while divisor < (initial_value/2) + 1:
+        print(divisor)
+        calculated = divide_by_input(dividend, divisor)
+        if calculated == -1:
+                divisor = divisor + 1
+        else:
+                dividend = calculated
+                list_of_factors.append(divisor)
+    print(list_of_factors)
+
 
 #divide the divident by the divisor. 
 #if clean division record factor, update divident
 #if !clean division, increment divisor
-def divide_by_input(dividend1, divisor2):
-    print("here")
-    if dividend1 % divisor2 == 0:
-        print("in if")
-        dividend = dividend1 / divisor2
-        print(divisor)
-        list_of_factors.append(divisor)
+def divide_by_input(dividend, divisor):
+    return_value = 0
+    if dividend % divisor == 0:
+        return_value = dividend / divisor
     else:
-        print("in else")
-        divisor = divisor + 1
-        print(divisor)
-
-
-def main():
-    while divisor < (dividend/2) + 1:
-        divide_by_input(dividend, divisor)
-        print(list_of_factors)
-        print(divisor)
+        return_value = -1
+    return return_value
 
 main()
